af2947f6d55913415d736b74433c255ce568b11f
Andrew <excisionhd1@gmail.com>
2018-10-13 16:57:42 -0700

    import sdl2

--- main.py
+++ main.py
@@ -2,17 +2,19 @@
 
 import cv2
 import pygame
+import sdl2
 
 width = 1920//2
 height = 1080//2
 
-screen = pygame.display.set_mode((width, height))
 
 def process_frame(img):
-    img = cv2.resize(img, (1920//2, 1080//2))
-    cv2.imshow('image', img)
-    cv2.waitKey(0)
-    print(img.shape)
+    img = cv2.resize(img, (width, height))
+
+    cv2.imshow('image',img)
+    #window.refresh()
+
+    #print(img.shape)
 
 if __name__ == "__main__":
     cap = cv2.VideoCapture("test.mp4")
