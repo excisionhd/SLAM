d4310257e8a35715c53e6da09ccca7a5e2a3ad2d
Andrew <excisionhd1@gmail.com>
2018-10-15 21:59:50 -0700

    update

--- display.py
+++ display.py
@@ -3,16 +3,10 @@
 import cv2
 import sdl2
 import sdl2.ext
-sdl2.ext.init()
 
-W = 1920//2
-H = 1080//2
-
-
-window.show()
-
-class Display(object):
+class Display(object, self):
     def __init__():
+        self.W, self.H = W, H
         self.window = sdl2.ext.Window("SLAM", size=(W,H), position=(100,100))
         self.window.show()
 
@@ -25,17 +19,3 @@ class Display(object):
         surf = sdl2.ext.pixels3d(window.get_surface())
         surf[:] = img[:,:]
         window.refresh()
-
-
-def process_frame(img):
-    img = cv2.resize(img, (W, H))
-
-if __name__ == "__main__":
-    cap = cv2.VideoCapture("test.mp4")
-
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if ret == True:
-            process_frame(frame)
-        else:
-            break
